e2a1e705ca9d02d0e33181ea6fd2f7ef8fe6fefe
zmorris248 <zmorris@coe.edu>
2020-10-02 15:30:31 -0500

    time

---
+++ Time.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _404NotFound
+{
+    class Time
+    {
+        private int start;
+        private int finish;
+
+
+
+
+
+        public Time(int ddttl)
+        {
+            this.start = ((ddttl / 10) % 100) / 2;
+            this.finish = start + (ddttl % 10) / 2;
+        }
+
+        public bool compareTimes(Time class1, Time class2)
+        {
+            if (class2.start < class1.finish && class1.finish < class2.finish) { return false; }
+            else if (class1.start < class2.finish && class2.finish < class1.finish) { return false; }
+            else if (class1.start == class2.start && class1.finish == class2.finish) { return false; }
+            else { return true; }
+        } 
+
+
+
+    }
+}
