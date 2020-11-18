d37e9f6bf0854a0cd777a9b8d6f40e83077b4e00
zmorris248 <zmorris@coe.edu>
2020-11-18 15:16:15 -0600

    Winning

--- Form16.Designer.cs
+++ Form16.Designer.cs
@@ -31,23 +31,27 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.Add = new System.Windows.Forms.Button();
             this.Remove = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(42, 48);
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(35, 100);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(277, 384);
+            this.listBox1.Size = new System.Drawing.Size(247, 308);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Add
             // 
-            this.Add.Location = new System.Drawing.Point(538, 59);
+            this.Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Add.Location = new System.Drawing.Point(478, 47);
+            this.Add.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Add.Name = "Add";
-            this.Add.Size = new System.Drawing.Size(163, 64);
+            this.Add.Size = new System.Drawing.Size(145, 51);
             this.Add.TabIndex = 1;
             this.Add.Text = "Add";
             this.Add.UseVisualStyleBackColor = true;
@@ -55,26 +59,42 @@
             // 
             // Remove
             // 
-            this.Remove.Location = new System.Drawing.Point(538, 162);
+            this.Remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Remove.Location = new System.Drawing.Point(478, 130);
+            this.Remove.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Remove.Name = "Remove";
-            this.Remove.Size = new System.Drawing.Size(163, 54);
+            this.Remove.Size = new System.Drawing.Size(145, 43);
             this.Remove.TabIndex = 2;
             this.Remove.Text = "Remove";
             this.Remove.UseVisualStyleBackColor = true;
             this.Remove.Click += new System.EventHandler(this.Remove_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Goldenrod;
+            this.label1.Font = new System.Drawing.Font("Courier New", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Admin Listings";
+            // 
             // Form16
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(730, 539);
+            this.ClientSize = new System.Drawing.Size(649, 431);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Remove);
             this.Controls.Add(this.Add);
             this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form16";
             this.Text = "Form16";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,5 +103,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button Add;
         private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Form17.Designer.cs
+++ Form17.Designer.cs
@@ -34,23 +34,28 @@
             this.Add_More = new System.Windows.Forms.Button();
             this.Finish = new System.Windows.Forms.Button();
             this.Arranged = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(43, 255);
+            this.comboBox1.Location = new System.Drawing.Point(26, 227);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(333, 28);
+            this.comboBox1.Size = new System.Drawing.Size(296, 24);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(446, 255);
+            this.comboBox2.Location = new System.Drawing.Point(452, 227);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(257, 28);
+            this.comboBox2.Size = new System.Drawing.Size(229, 24);
             this.comboBox2.TabIndex = 1;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
@@ -58,17 +63,20 @@
             // 
             this.checkedListBox1.CheckOnClick = true;
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(78, 25);
+            this.checkedListBox1.Location = new System.Drawing.Point(26, 62);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(280, 119);
+            this.checkedListBox1.Size = new System.Drawing.Size(249, 106);
             this.checkedListBox1.TabIndex = 7;
             this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
             // 
             // Add_More
             // 
-            this.Add_More.Location = new System.Drawing.Point(548, 38);
+            this.Add_More.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Add_More.Location = new System.Drawing.Point(345, 20);
+            this.Add_More.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Add_More.Name = "Add_More";
-            this.Add_More.Size = new System.Drawing.Size(167, 50);
+            this.Add_More.Size = new System.Drawing.Size(148, 40);
             this.Add_More.TabIndex = 8;
             this.Add_More.Text = "Add More";
             this.Add_More.UseVisualStyleBackColor = true;
@@ -76,9 +84,11 @@
             // 
             // Finish
             // 
-            this.Finish.Location = new System.Drawing.Point(548, 114);
+            this.Finish.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Finish.Location = new System.Drawing.Point(533, 22);
+            this.Finish.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Finish.Name = "Finish";
-            this.Finish.Size = new System.Drawing.Size(167, 45);
+            this.Finish.Size = new System.Drawing.Size(148, 36);
             this.Finish.TabIndex = 9;
             this.Finish.Text = "Finish";
             this.Finish.UseVisualStyleBackColor = true;
@@ -86,29 +96,69 @@
             // 
             // Arranged
             // 
-            this.Arranged.Location = new System.Drawing.Point(548, 184);
+            this.Arranged.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Arranged.Location = new System.Drawing.Point(533, 93);
+            this.Arranged.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Arranged.Name = "Arranged";
-            this.Arranged.Size = new System.Drawing.Size(167, 46);
+            this.Arranged.Size = new System.Drawing.Size(148, 37);
             this.Arranged.TabIndex = 10;
             this.Arranged.Text = "ARRANGED";
             this.Arranged.UseVisualStyleBackColor = true;
             this.Arranged.Click += new System.EventHandler(this.Arranged_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Goldenrod;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(22, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 23);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Start Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Goldenrod;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(448, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Goldenrod;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(22, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Days of the Week";
+            // 
             // Form17
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Arranged);
             this.Controls.Add(this.Finish);
             this.Controls.Add(this.Add_More);
             this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form17";
             this.Text = "Form17";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -120,5 +170,8 @@
         private System.Windows.Forms.Button Add_More;
         private System.Windows.Forms.Button Finish;
         private System.Windows.Forms.Button Arranged;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
